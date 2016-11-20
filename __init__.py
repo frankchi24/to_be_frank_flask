668eabe4ffee04ec99d4bd5097ccc7c800573a72
frankchi24 <frankchi24@gmail.com>
2016-11-20 22:47:51 +0800

    for loop,debug message,content management file

--- __init__.py
+++ __init__.py
@@ -1,26 +1,34 @@
 from flask import Flask, render_template
+from content_management import Content
+
+TOPIC_DIC = Content()
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template("main.html")
+    return render_template("main.html",TOPIC_DIC = TOPIC_DIC)
 
-@app.route('/about')
+@app.route('/about/')
 def about():
     return render_template("/about.html")
 
-@app.route('/project')
+@app.route('/project/')
 def project():
     return render_template("/project.html")
 
-@app.route('/contact')
+@app.route('/contact/')
 def contact():
     return render_template("/contact.html")
 
-@app.route('/post')
+@app.route('/post/')
 def post():
     return render_template("/post.html")
+
+
+app.config.update(
+    PROPAGATE_EXCEPTIONS = True
+)
 
 if __name__ == "__main__":
     app.run()
---
+++ content_management.py
@@ -0,0 +1,18 @@
+
+
+def Content():
+	TOPIC_DIC = [
+	{
+	"title":"Python Flask and Jinjia Tutorial",
+	"subtitle":"To become one of the few who can code is not cool",
+	"url":"/first_post/",
+	"author":"Frank"
+	},
+	{
+	"title":"Thoughts on Netflix and Amazon",
+	"subtitle":"Not sure about the first week",
+	"url":"/second_post/",
+	"author":"Frank"
+	}
+	]
+	return TOPIC_DIC
